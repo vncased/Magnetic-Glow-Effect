56296f0a4504a3e0c2d1a702a740e6ff7a939942
kliscle <lachin.akhmetov@gmail.com>
2025-01-21 12:28:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import platform
 system_name = platform.uname().system 
 
 if system_name == "Linux":
-    os.system('python3 tll.py')
+    os.system('python3 src/tll.py')
 elif system_name == "Windows":
-    os.system("python3 tll.py")
+    os.system("python3 src/tll.py")
