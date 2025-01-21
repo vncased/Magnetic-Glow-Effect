9daf13431caf20d4cc6107bc5a746f5341d3dda6
kliscle <lachin.akhmetov@gmail.com>
2025-01-21 12:24:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import platform
+
+system_name = platform.uname().system 
+
+if system_name == "Linux":
+    os.system('python3 tll.py')
+elif system_name == "Windows":
+    os.system("python3 tll.py")
